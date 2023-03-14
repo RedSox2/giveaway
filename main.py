35600ab8ec04e67b8595ea93a2028a07140666e4
RedSox2 <65141925+RedSox2@users.noreply.github.com>
2023-03-14 22:10:15 +0000

    edit main.py

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ L = instaloader.Instaloader()
 
 post = instaloader.Post.from_shortcode(L.context, 'CpusuF5Oi1c')
 
+print(post.owner_username)
 
-print(post.owner_profile.username)
+for comment in post.get_comments():
+    print(comment)
+    break
